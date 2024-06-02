146c5034a1e906e8876b147a4b2dd14244372a4f
Lương Văn Linh <91690292+lvlinh45@users.noreply.github.com>
2024-06-02 23:21:06 +0700

    Update MainActivity.java

---
+++ app/src/main/java/linh/edu/moviesapp/Activities/MainActivity.java
@@ -0,0 +1,73 @@
+package linh.edu.moviesapp.Activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import linh.edu.moviesapp.Adapters.FilmListAdapter;
+import linh.edu.moviesapp.Adapters.SlidersAdapter;
+import linh.edu.moviesapp.Domains.Film;
+import linh.edu.moviesapp.Domains.SliderItems;
+import linh.edu.moviesapp.R;
+import linh.edu.moviesapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new ExplorerFragment());
+        binding.bottomNavigationMenu.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.favorites) {
+                replaceFragment(new FavoriteFragment());
+            }
+            if(item.getItemId() == R.id.explorer) {
+                replaceFragment(new ExplorerFragment());
+            }
+            if(item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            }
+            if(item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+
+        // giúp layout của cửa sổ vượt ra ngoài giới hạn thông thường của màn hình.
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
